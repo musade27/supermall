a071b0fe340fac39e8c85108dd72d7a8cac9f48f
musade27 <779412847@qq.com>
2021-07-23 15:09:43 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 </template>
 
 <script>
-export default {
-  name: 'App',
-  components: {
-    
+  export default {
+    name: 'App',
+    components: {
+      
+    }
   }
-}
 </script>
 
 <style>
